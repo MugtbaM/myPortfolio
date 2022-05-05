a20f190910a5407210221758ca8e70d41aa5c452
Mugtba Mirghani <99028356+MugtbaM@users.noreply.github.com>
2022-05-05 09:08:57 -0700

    removed attributes in phones screens

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function myFunction() {
   }
   /* removing animation in small size screens */
 
-  const mqList = window.matchMedia("(max-width: 600px)")
+  const mqList = window.matchMedia("(max-width: 600px)");
 // If media query matches on load
 if (mqList.matches) {
-    document.querySelectorAll(".image-container-flex").removeAttribute("data-aos");
+    document.getElementById("imgflex1").removeAttribute("data-aos");
+    document.getElementById("imgflex2").removeAttribute("data-aos");
+    document.getElementById("imgflex3").removeAttribute("data-aos");
 }
 // If media query matches after resize
 // mqList.addListener(function(mql) {
